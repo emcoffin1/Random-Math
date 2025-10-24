38ff941854e3400772b2c61253f95d87a9106cfe
Ethan Coffin <emcoffin@gmail.com>
2025-10-24 00:08:47 -0700

    Added bartz heat transfer

--- HeatTransfer/bartz_formuals.py
+++ HeatTransfer/bartz_formuals.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def bartz_heat_transfer(gamma, R, Pc, rt, x, y, cp, k, mu, Tc, T, M, t_wall=0.002, k_wall=30, T_cool=500, h_c=15000,
-                        Tw=900, Pr=0.9, w=0.7):
+                        Tw=900, w=0.7):
     """
     Uses Bartz Formula to compute the gas-side convective heat transfer coefficient
     Essentially shows you how efficiently the hot gas transfers heat to the wall
@@ -23,13 +23,14 @@ def bartz_heat_transfer(gamma, R, Pc, rt, x, y, cp, k, mu, Tc, T, M, t_wall=0.00
     :return:
     """
     # Variable Calcs and Assumptions
-    c_star = np.sqrt(R * Tc) / gamma * (2/ (gamma + 1))**-((gamma + 1)/2/(gamma - 1))
+    c_star = (1/np.sqrt(gamma)) * ((2/(gamma + 1))**((gamma + 1)/(2*(gamma - 1)))) * np.sqrt(R * Tc)
+
     D = 2*y
     Dt = 2*rt
 
     # Initial wall temperature guess (through full engine)
-    T_wall_inner = np.ones_like(x) * 800
-    T_wall_innter_old = T_wall_inner.copy()
+    T_wall_inner = np.ones_like(x) * Tw
+    T_wall_inner_old = T_wall_inner.copy()
     T_wall_outer = np.zeros_like(x) * T_cool
 
     for i in range(1):
@@ -59,7 +60,7 @@ def bartz_heat_transfer(gamma, R, Pc, rt, x, y, cp, k, mu, Tc, T, M, t_wall=0.00
         """
         r = (mu*cp/k)**(1/3)
         Taw = T*(1 + r*(gamma-1)/2*M**2)
-        q_dot = (Taw - T_cool) / R
+        q_dot = (Taw - T_cool) / R_total
 
         """Now solve for the wall temperatures, remembering that qdot = h(Taw-Tw)
         and qdot = h_c(Tw,o - T_air) --> this is the convection portion
